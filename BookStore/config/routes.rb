bbb3e266ebd3aa9f0dcbd8bad0f2349489df91bf
ryansmpsn <ryzizn@gmail.com>
2017-11-15 00:28:42 -0600

    Add files via upload

--- BookStore/config/routes.rb
+++ BookStore/config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root 'home_page#index'
-   get 'book/list'
-   get 'book/new'
-   post 'book/create'
-   patch 'book/update'
-   get 'book/list'
-   get 'book/show'
-   get 'book/edit'
-   get 'book/delete'
-   get 'book/update'
+  root to: 'pages#home'
+
+  resources :invoices
+  resources :orders
+  resources :customers
+  resources :employees
+  resources :books
+  get 'pages/home'
+
+  get 'pages/about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
